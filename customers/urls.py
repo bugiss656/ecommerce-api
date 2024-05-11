cde7036df185b96ce736f8eacbc5fd195ad8d23a
mbugiel656 <mbugiel656@gmail.com>
2024-05-11 10:17:34 +0200

    Add CheckPasswordView view for checking if password is correct

--- customers/urls.py
+++ customers/urls.py
@@ -3,6 +3,7 @@ from .views import (
     RegisterUserView,
     CreateTokenView,
     ManageUserView,
+    CheckPasswordView,
     UpdateProfileView
 )
 
@@ -13,5 +14,6 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('token/', CreateTokenView.as_view(), name='token'),
     path('user/', ManageUserView.as_view(), name='user'),
-    path('update/<int:pk>', UpdateProfileView.as_view(), name='update')
+    path('update/<int:pk>', UpdateProfileView.as_view(), name='update'),
+    path('check-password/<int:user_id>', CheckPasswordView.as_view(), name='check_password')
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,11 @@
 from rest_framework import (
+    views,
     generics,
     authentication,
-    permissions
+    permissions,
+    status
 )
+from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from .serializers import (
@@ -10,6 +13,7 @@ from .serializers import (
     AuthTokenSerializer
 )
 from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
 from .models import User
 
 
@@ -31,6 +35,20 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
     
+
+class CheckPasswordView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id, *args, **kwargs):
+        user = get_object_or_404(User, id=user_id)
+
+        provided_password = request.data.get('password')
+
+        if user.check_password(provided_password):
+            return Response({'message': 'Password is correct'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Password is incorrect'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UpdateProfileView(generics.UpdateAPIView):
     queryset = User.objects.all()
